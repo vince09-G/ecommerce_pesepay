7f4e59c2b0c128074729e21556110ff2dcac8603
unknown <wonderfulkadani@gmail.com>
2026-09-15 22:39:05 +0200

    Remooved debug toolbar

--- storefront/urls.py
+++ storefront/urls.py
@@ -18,7 +18,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
